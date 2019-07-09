a4689ea19b502666ada83a22740cc38740e06777
BayuPrasetyo0234 <Bayu.Prasetyo.0456@gmail.com>
2019-07-09 14:00:48 +0700

    Add Repository CategoryAbility

---
+++ BE/src/main/java/com/example/dto/repository/AbilityCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.dto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.dto.model.AbilityCategoryModel;
+
+public interface AbilityCategoryRepository extends JpaRepository<AbilityCategoryModel, Long>{
+
+}
