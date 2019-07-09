c5144925d2cbaaada170c7885a5cae9cce72e2fe
ekaapsd <epdewi26@gmail.com>
2019-07-09 17:02:44 +0700

    Update repository add query

--- BE/src/main/java/com/example/dto/repository/ParameterRepository.java
+++ BE/src/main/java/com/example/dto/repository/ParameterRepository.java
@@ -1,9 +1,13 @@
 package com.example.dto.repository;
+//diedit sama Eka
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.dto.model.ParameterModel;
 
 public interface ParameterRepository extends JpaRepository<ParameterModel, Long>{
-
+	@Query(value="SELECT * FROM parameter ORDER BY TB_PARAMETER DESC", nativeQuery=true)
+	List<ParameterModel> getLatest();
 }
